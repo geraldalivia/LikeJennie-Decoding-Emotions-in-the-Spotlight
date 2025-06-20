c0173d76f4150e4ff9d82d879032ab2e360608f9
Geralda Livia Nurgaha <93833681+geraldalivia@users.noreply.github.com>
2025-06-20 13:29:45 +0700

    Delete jennie_noapi.py

--- jennie_noapi.py
+++
@@ -1,3 +0,0 @@
-from youtube_comment_downloader import YoutubeCommentDownloader
-downloader = YoutubeCommentDownloader()
-comments = downloader.get_comments_from_url('https://www.youtube.com/watch?v=JSFG-IE8n_c')
